c585a540de309e2c4029e5baaa52918819761c51
Bruno Coelho <bbcoelho@gmail.com>
2025-07-31 16:56:41 -0300

    chore: add typescript

---
+++ mongooseBasics/src/index.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+// use `mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+  .then(() => {
+    console.log('CONNECTED TO DB');
+  })
+  .catch(err => {
+    console.error('Error connecting to DB:', err);
+  });
