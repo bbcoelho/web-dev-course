5689b10671387beccef96efdaf8c560635a9b36c
Bruno Coelho <bbcoelho@gmail.com>
2025-08-04 19:23:02 -0300

    chore: add ejs

--- yelpCamp/src/app.ts
+++ yelpCamp/src/app.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('home');
 });
 
 // use `mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
