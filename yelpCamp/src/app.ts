4b6cf7dc7a0d3e31b9b4d7193e18ca6fcfe2e78e
Bruno Coelho <bbcoelho@gmail.com>
2025-08-04 19:06:21 -0300

    chore: add build and dev scripts

--- yelpCamp/src/app.ts
+++ yelpCamp/src/app.ts
@@ -1,6 +1,16 @@
-// import mongoose from 'mongoose';
+import express from 'express';
+import mongoose from 'mongoose';
 
-// // use `mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+const app = express();
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// use `mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 // mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
 //     .then(() => {
 //         console.log('CONNECTED TO DB');
