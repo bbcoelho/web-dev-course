87524cf2cd5a073d3f69f7f275e6d61c46c0af4b
Bruno Coelho <bbcoelho@gmail.com>
2025-09-02 17:48:17 -0300

    chore: add script to open mongo shell

--- yelpCamp/src/app.ts
+++ yelpCamp/src/app.ts
@@ -3,17 +3,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import mongoose from 'mongoose';
+import { Campground } from './models/campground.js';
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://mongodb:27017/yelpCamp';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/yelpCamp';
+mongoose.connect(MONGO_URI);
 
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('CONNECTED TO DB:', MONGO_URI);
-    })
-    .catch(err => {
-        console.error('Error connecting to DB:', err);
-    });
-    
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'CONNECTION ERROR:'));
+db.once('open', () => {
+    console.log('CONNECTED TO DB');
+});
+
 const app = express();
 
 // Get __dirname equivalent in ES modules
@@ -24,9 +24,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('SERVER IS RUNNING ON PORT 3000');
 });
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const campground = new Campground({
+        name: 'My Backyard',
+        price: 100,
+        description: 'This is a campground in my backyard',
+        location: 'This is a location'
+    });
+    await campground.save();
+    res.send(campground);
 });
