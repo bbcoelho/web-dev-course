bbcc781c897906fe7625aac522994ee3fd478d64
Bruno Coelho <bbcoelho@gmail.com>
2025-09-09 23:38:00 -0300

    test: editing campground

---
+++ yelpCamp/tests/edit.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('editing campground', async ({ page }) => {
+	await page.goto('http://localhost:3000/campgrounds');
+	await page.getByRole('link', { name: 'Salmon Creek' }).click();
+	await page.getByRole('link', { name: 'Edit' }).click();
+	await page.getByRole('textbox', { name: 'Name' }).click();
+	await page.getByRole('textbox', { name: 'Name' }).press('ArrowRight');
+	await page.getByRole('textbox', { name: 'Name' }).press('ArrowRight');
+	await page.getByRole('textbox', { name: 'Name' }).press('ControlOrMeta+Shift+ArrowLeft');
+	await page.getByRole('textbox', { name: 'Name' }).fill('Salmon Creek');
+	await page.getByRole('textbox', { name: 'Location' }).click();
+	await page.getByRole('textbox', { name: 'Location' }).fill('Montana');
+	await page.getByRole('button', { name: 'Save Changes' }).click();
+
+	// expect page to have title "Show"
+	await expect(page).toHaveTitle(/Show/);
+	// expect page to redirect to show page with the id of the campground "http://localhost:3000/campgrounds/[a-f0-9]{24}"
+	await expect(page).toHaveURL(/http:\/\/localhost:3000\/campgrounds\/[a-f0-9]{24}/)
+});
