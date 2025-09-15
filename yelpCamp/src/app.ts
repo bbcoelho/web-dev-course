9c9b96133e1f9bd36e06df7f5eebc727d3916901
Bruno Coelho <bbcoelho@gmail.com>
2025-09-15 14:03:16 -0300

    fix: add public folder to serve assets

--- yelpCamp/src/app.ts
+++ yelpCamp/src/app.ts
@@ -33,7 +33,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'views/images')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('SERVER IS RUNNING ON PORT 3000');
