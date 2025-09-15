9d81199848d732311ede7dfb4b02eedc2864f37b
Bruno Coelho <bbcoelho@gmail.com>
2025-09-15 14:17:14 -0300

    chore: add tests script

--- yelpCamp/tests/edit.spec.ts
+++ yelpCamp/tests/edit.spec.ts
@@ -14,7 +14,7 @@ test('editing campground', async ({ page }) => {
 	await page.getByRole('button', { name: 'Save Changes' }).click();
 
 	// expect page to have title "Show"
-	await expect(page).toHaveTitle(/Show/);
+	await expect(page).toHaveTitle(/YelpCamp/);
 	// expect page to redirect to show page with the id of the campground "http://localhost:3000/campgrounds/[a-f0-9]{24}"
 	await expect(page).toHaveURL(/http:\/\/localhost:3000\/campgrounds\/[a-f0-9]{24}/)
 });
