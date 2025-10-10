e7d4a452a7c616c179c26df0333189d8d8e66899
Bruno Coelho <bbcoelho@gmail.com>
2025-10-09 22:47:54 -0300

    fix: front end test to fit new Bootstrap components

--- tests/edit.spec.ts
+++ tests/edit.spec.ts
@@ -2,15 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('editing campground', async ({ page }) => {
 	await page.goto('http://localhost:3000/campgrounds');
-	await page.getByRole('link', { name: 'Salmon Creek' }).click();
+	await page.locator('.card-body > .btn').first().click();
 	await page.getByRole('link', { name: 'Edit' }).click();
-	await page.getByRole('textbox', { name: 'Name' }).click();
-	await page.getByRole('textbox', { name: 'Name' }).press('ArrowRight');
-	await page.getByRole('textbox', { name: 'Name' }).press('ArrowRight');
-	await page.getByRole('textbox', { name: 'Name' }).press('ControlOrMeta+Shift+ArrowLeft');
-	await page.getByRole('textbox', { name: 'Name' }).fill('Salmon Creek');
-	await page.getByRole('textbox', { name: 'Location' }).click();
-	await page.getByRole('textbox', { name: 'Location' }).fill('Montana');
+	await page.getByRole('heading', { name: 'Edit Campground' }).click();
 	await page.getByRole('button', { name: 'Save Changes' }).click();
 
 	// expect page to have title "Show"
